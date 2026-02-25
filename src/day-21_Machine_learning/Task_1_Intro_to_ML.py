5268317cbeda0d2a367d7566808017dd3a1b13a9
Abhishek <abhi.808802@gmail.com>
2026-02-25 15:35:48 +0530

    Initialisation Commited

---
+++ src/day-21_Machine_learning/Task_1_Intro_to_ML.py
@@ -0,0 +1,7 @@
+Machine_Learning = """
+* Machine Learning (ML) is a way to make computers learn from data.
+* It helps computers find patterns and make decisions without being clearly programmed.
+* The more data it gets, the better it can improve.
+* It is used in things like YouTube recommendations, Google search, and face unlock.
+"""
+print(Machine_Learning)
---
+++ src/day-21_Machine_learning/Task_2_branches_of_ML.py
@@ -0,0 +1,23 @@
+supervised_learning = """
+Supervised Learning :
+    It uses labeled data, meaning the correct answers are already given, like spam detection or house price prediction.
+"""
+unsupervised_learning = """
+Unsupervised learning :
+    It uses unlabeled data and finds hidden patterns, like grouping customers.
+"""
+Reinforcement_learning = """
+Reinforcement learning :
+    It learns through rewards and penalties, like training a self-driving car or game AI.
+"""
+
+Difference = """
+            Supervised Learning                      |        Unsupervised learning                    |        Reinforcement learning
+* Data has correct answers                           | * Data has no labels or answers.                |  * Learns using rewards and penalties.
+* Used for prediction and classification.            | * Used to find hidden patterns or groups.       |  * Works by trial and error.
+* Example: Spam detection, house price prediction.   | * Example: Customer segmentation, clustering.   |  * Example: Self-driving cars, game playing AI.
+"""
+print(supervised_learning)
+print(unsupervised_learning)
+print(Reinforcement_learning)
+print(Difference)
